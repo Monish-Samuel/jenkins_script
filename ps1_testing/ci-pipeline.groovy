a91b9d8435b84fd3098c5ad7e4f2461725e722e6
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-09-12 16:50:00 +0530

    Update and rename Jenkinsfile to ci-pipeline.groovy

--- ps1_testing/ci-pipeline.groovy
+++ ps1_testing/ci-pipeline.groovy
@@ -40,23 +40,6 @@ agent any
 			)
 		    }
 	    }
-	    stage('Artifact-Download'){
-		    steps{
-			    rtDownload (
-   				 serverId: 'generic-libs-prod',
-    					spec: '''{
-          					"files": [
-            						{
-              						"pattern": "generic-libs-prod/app-$buildNo.zip",
-             						 "target": "./artifacts/"
-           				 		}
-          					]
-   					 }''',
-				    buildName: 'Flask-App',
-				    buildNumber: buildNo,
-				)
-		    }
-	    }
 	    stage('Deployment'){
 		    steps{
 	    		build job: 'ps1_testing/cd-pipeline', parameters: [

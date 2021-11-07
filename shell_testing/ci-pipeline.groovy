e261ba454b1082610c511a94c5fb5d2367fa651a
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 15:22:07 +0530

    Update ci-pipeline.groovy

--- shell_testing/ci-pipeline.groovy
+++ shell_testing/ci-pipeline.groovy
@@ -5,13 +5,15 @@ agent any
             steps{
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Monish-Samuel/shell_testing.git']]])
 		    script{
-		    	def props = readProperties  file:'/var/lib/jenkins/workspace/shell_testing/ci-pipeline/build.properties'
-			def majorver= props['MAJOR_VERSION']
-			def minorver= props['MINOR_VERSION']
-			def patchver= props['PATCH_VERSION']    
-			def gitbranch= props['BRANCH_NAME']
-			    def buildNumber= majorver + "-" + minorver + "-" + patchver + "-" + BUILD_NUMBER
-			    echo "${buildNumber}"
+		    	    def props = readProperties  file:'/var/lib/jenkins/workspace/shell_testing/ci-pipeline/build.properties'
+			    def majorver= props['MAJOR_VERSION']
+			    def minorver= props['MINOR_VERSION']
+			    def patchver= props['PATCH_VERSION']    
+			    def gitbranch= props['BRANCH_NAME']
+			    def buildNumber= majorver + "." + minorver + "." + patchver + "." + BUILD_NUMBER
+			    currentBuild.displayName = "${buildNumber}"
+			    currentBuild.description= "${gitbranch}"
+			    env.buildNo= buildNumber
 		    }
 	    }	
 	}
@@ -34,19 +36,19 @@ agent any
 //     					spec: '''{
 //           					"files": [
 //             						{
-//               						"pattern": "./myapp-$BUILD_NUMBER.zip",
+//               						"pattern": "./myapp-$buildNo.zip",
 //              						 "target": "zip-libs-prod/"
 //            				 		}
 //           					]
 //    					 }''',
 // 				    buildName: 'Flask-App',
-// 				    buildNumber: BUILD_NUMBER,
+// 				    buildNumber: buildNo,
 // 				)
 			    			    
 // 			rtPublishBuildInfo (
 //     				serverId: 'zip-libs-prod',
 // 				buildName: 'Flask-App',
-// 				buildNumber: BUILD_NUMBER,
+// 				buildNumber: buildNo,
 // 			)
 // 		    }
 // 	    }

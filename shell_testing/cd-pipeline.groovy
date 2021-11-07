f314701277aab180ab1387a6042d4c461b590960
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 15:28:49 +0530

    Create cd-pipeline.groovy

---
+++ shell_testing/cd-pipeline.groovy
@@ -0,0 +1,29 @@
+pipeline{
+agent any
+    stages{	    
+      stage('Artifact-Download'){
+		    steps{
+			    //Initial Clean up of folder before downloading artifact
+			    sh 'find . -name "*.zip" -type f -delete'
+			    rtDownload (
+   				 serverId: 'zip-libs-prod',
+    					spec: '''{
+          					"files": [
+            						{
+              						"pattern": "zip-libs-prod/myapp-$buildVersion.zip",
+             						 "target": "."
+           				 		}
+          					]
+   					 }''',
+				    buildName: 'Flask-App',
+				    buildNumber: buildVersion,
+				)
+		    }
+	    }
+      stage ('Unzipping Artiffact'){
+        steps{
+        sh 'unzip myapp-$buildVersion.zip'
+        }
+      }
+    }
+}

34c3900c482ed2b30bb1dc46c08b9e79695353b8
Monish-Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2021-11-07 14:20:03 +0530

    Update ci-pipeline.groovy

--- shell_testing/ci-pipeline.groovy
+++ shell_testing/ci-pipeline.groovy
@@ -4,56 +4,47 @@ agent any
         stage('Pre-Flight Checks'){
             steps{
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/Monish-Samuel/shell_testing.git']]])
-			script{
-			sh 'pwsh'
-			def file= powershell returnStdout: true, script: './build_scripts/build_prep.ps1'
-			def buildNumber= powershell (returnStdout: true, script: 'Get-Content ./buildNo.txt').trim()
-			currentBuild.displayName = "${buildNumber}"
-			env.buildNo= buildNumber
-			def buildbranch= powershell (returnStdout: true, script: 'Get-Content ./buildbranch.txt').trim()
-			currentBuild.description= "${buildbranch}"
-		}
 	    }	
 	}
 	    stage('Build-Automation'){
 		    steps{
-			    powershell './build_scripts/zip_creation.ps1'
+			    sh 'zip myapp-$BUILD_NUMBER.zip ./src'
 		    }
 	    }
 	    stage('Code Analysis'){
 		    steps{
-			    powershell './code_analysis/analysis.ps1'
+			    sh './code_analysis/analysis.sh'
 		    }
 	    }
 	    stage('Build-Management'){
 		    steps{
 			    rtUpload (   
-   				 serverId: 'generic-libs-prod',
+   				 serverId: 'zip-libs-prod',
     					spec: '''{
           					"files": [
             						{
-              						"pattern": "./source-repo/app-$buildNo.zip",
-             						 "target": "generic-libs-prod/"
+              						"pattern": "./source-repo/app-$BUILD_NUMBER.zip",
+             						 "target": "zip-libs-prod/"
            				 		}
           					]
    					 }''',
 				    buildName: 'Flask-App',
-				    buildNumber: buildNo,
+				    buildNumber: BUILD_NUMBER,
 				)
 			    			    
 			rtPublishBuildInfo (
     				serverId: 'generic-libs-prod',
 				buildName: 'Flask-App',
-				buildNumber: buildNo,
+				buildNumber: BUILD_NUMBER,
 			)
 		    }
 	    }
-	    stage('Deployment'){
-		    steps{
-	    		build job: 'ps1_testing/cd-pipeline', parameters: [
-				string(name: 'buildVersion', value: buildNo)
-                ]
-		    }
-	    }
+// 	    stage('Deployment'){
+// 		    steps{
+// 	    		build job: 'ps1_testing/cd-pipeline', parameters: [
+// 				string(name: 'buildVersion', value: buildNo)
+//                 ]
+// 		    }
+// 	    }
     }
 }

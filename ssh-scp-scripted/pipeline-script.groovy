e3346226c77328d976a9906f33c631b296b0e921
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-01-29 19:53:27 +0530

    Update pipeline-script.groovy

--- ssh-scp-scripted/pipeline-script.groovy
+++ ssh-scp-scripted/pipeline-script.groovy
@@ -58,7 +58,7 @@ def execute(){
 			writeFile file: "${env.WORKSPACE}/india-server.pem", text: readFile(my_private_key);
 			sh "chmod 400 ${env.WORKSPACE}/india-server.pem";
 			sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com 'ls -lRt'"
-	        	sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com [ -e myapp.zip ] && rm -- myapp.zip"
+	        	sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com '[ -e myapp.zip ] && rm -- myapp.zip'"
 			sh "scp -i ${env.WORKSPACE}/india-server.pem -r ${env.WORKSPACE}/shell_testing/myapp.zip ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com:~/"	
 		}
 	}

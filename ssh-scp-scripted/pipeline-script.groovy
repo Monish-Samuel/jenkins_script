cad63f636f34fdc279927b2732a908e7ec016d0b
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-01-29 19:51:29 +0530

    Update pipeline-script.groovy

--- ssh-scp-scripted/pipeline-script.groovy
+++ ssh-scp-scripted/pipeline-script.groovy
@@ -55,12 +55,11 @@ def execute(){
 		
 		withCredentials([file(credentialsId: 'india-server.pem', variable: 'my_private_key')]){
 			sh "mv ${env.WORKSPACE}/shell_testing/myapp-${buildNo}.zip ${env.WORKSPACE}/shell_testing/myapp.zip"
-			sh 'cd .. && ls -lRt'
 			writeFile file: "${env.WORKSPACE}/india-server.pem", text: readFile(my_private_key);
-			sh 'cd .. && ls -lRt'
-			sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com"
+			sh "chmod 400 ${env.WORKSPACE}/india-server.pem";
+			sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com 'ls -lRt'"
 	        	sh "ssh -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com [ -e myapp.zip ] && rm -- myapp.zip"
-			sh "scp -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem myapp.zip ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com:/"	
+			sh "scp -o StrictHostKeyChecking=no -i ${env.WORKSPACE}/india-server.pem -r ${env.WORKSPACE}/shell_testing/myapp.zip ec2-user@ec2-13-232-137-52.ap-south-1.compute.amazonaws.com:~/"	
 		}
 	}
   

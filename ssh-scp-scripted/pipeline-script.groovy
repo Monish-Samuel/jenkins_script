7a34c9789e0eca74fb6e678db3cede4d49c2f8c0
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-01-29 17:20:34 +0530

    Update pipeline-script.groovy

--- ssh-scp-scripted/pipeline-script.groovy
+++ ssh-scp-scripted/pipeline-script.groovy
@@ -1,5 +1,6 @@
 node{
   try{
+   cloneRepo('master')
    execute();
   }catch(Exception e){
     println ("Error executing pipeline");
@@ -13,26 +14,21 @@ node{
 def execute(){
   
   def propsFile= 'shell_testing/build.properties'
-  env.WORKSPACE= pwd()
   def props = readProperties  file:propsFile
-	def majorver= props['MAJOR_VERSION']
-	def minorver= props['MINOR_VERSION']
-	def patchver= props['PATCH_VERSION']    
-	def gitbranch= props['BRANCH_NAME']
-	def buildNumber= majorver + "." + minorver + "." + patchver + "." + BUILD_NUMBER
-	currentBuild.displayName = "${buildNumber}"
-	currentBuild.description= "${gitbranch}"
-	env.buildNo= buildNumber
+  def majorver= props['MAJOR_VERSION']
+  def minorver= props['MINOR_VERSION']
+  def patchver= props['PATCH_VERSION']    
+  def gitbranch= props['BRANCH_NAME']
+  def buildNumber= majorver + "." + minorver + "." + patchver + "." + BUILD_NUMBER
+  currentBuild.displayName = "${buildNumber}"
+  currentBuild.description= "${gitbranch}"
+  env.buildNo= buildNumber
+	
+	stage('Build-Automation'){
+		sh "chmod +x -R ${env.WORKSPACE}"
+		sh './build_scripts/zip_creation.sh'
+	    }
   
-  stage("Clone-Repo"){
-    try{
-      cloneRepo(gitbranch);
-      println ("Cloned repo successfully")
-    }catch(Exception e){
-      println ("Error Cloning Repo")
-      throw e;
-    }
-  }
 }
 
 def cloneRepo(branchName){

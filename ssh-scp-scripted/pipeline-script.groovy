55564403cef8f0c1151659dbced21da90924ecb9
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-01-30 13:11:31 +0530

    Update pipeline-script.groovy

--- ssh-scp-scripted/pipeline-script.groovy
+++ ssh-scp-scripted/pipeline-script.groovy
@@ -34,9 +34,9 @@ def execute(){
 	stage('Sonar Quality and Gate'){
 		def SONARSCANNER = tool "sonar-scanner";
             	withSonarQubeEnv("sonar") {
-              	sh "${SONARSCANNER}/sonar-scanner"
-		timeout(time: 1, unit: 'MINUTES') {
-                    waitForQualityGate abortPipeline: true
+			sh "cd ${env.WORKSPACE}/shell_testing ${SONARSCANNER}/sonar-scanner"
+			timeout(time: 1, unit: 'MINUTES') {
+                    		waitForQualityGate abortPipeline: true
                 	}
 		}
 	}
